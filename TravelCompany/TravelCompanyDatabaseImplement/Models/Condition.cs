21e9236ff11361d5f79a095b4a33c2d249fa600e
Malkova Anastasia <ad.malkova73@gmail.com>
2022-02-26 20:09:04 +0400

    BaseLab3

---
+++ TravelCompany/TravelCompanyDatabaseImplement/Models/Condition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TravelCompanyDatabaseImplement.Models
+{
+    public class Condition
+    {
+        public int Id { get; set; }
+        [Required]
+        public string ConditionName { get; set; }
+        [ForeignKey("ConditionId")]
+        public virtual List<TravelCondition> TravelConditions { get; set; }
+    }
+}
---
+++ TravelCompany/TravelCompanyDatabaseImplement/TravelCompanyDatabase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelCompanyDatabaseImplement.Models;
+
+namespace TravelCompanyDatabaseImplement
+{
+    public class TravelCompanyDatabase : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=TravelCompanyDatabase;Username=postgres;Password=MADScorpion");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+        public virtual DbSet<Condition> Conditions { set; get; }
+        public virtual DbSet<Travel> Travels { set; get; }
+        public virtual DbSet<TravelCondition> TravelConditions { set; get; }
+        public virtual DbSet<Order> Orders { set; get; }
+    }
+}
