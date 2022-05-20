a63683050c5e07a9e9e83fb9f605bceb221fcd5d
Malkova Anastasia <ad.malkova73@gmail.com>
2022-05-20 23:52:05 +0400

    CompLab8 ready

--- TravelCompany/TravelCompanyView/FormMessages.cs
+++ TravelCompany/TravelCompanyView/FormMessages.cs
@@ -31,8 +31,9 @@ namespace TravelCompanyView
                 {
                     ToSkip = currentPage * page_size,
                     ToTake = page_size + 1
-                });
-                hasNext = !(list.Count <= page_size);
+                }), dataGridView);
+                PageLabel.Text = currentPage.ToString();
+                hasNext = !(dataGridView.Rows.Count <= page_size);
                 if (hasNext)
                 {
                     ButtonNext.Text = "Next " + (currentPage + 2);
@@ -42,10 +43,6 @@ namespace TravelCompanyView
                 {
                     ButtonNext.Text = "Next";
                     ButtonNext.Enabled = false;
-                }
-                if (list != null)
-                {
-                    dataGridView.DataSource = list.Take(page_size).ToList();
                 }
             }
             catch (Exception ex)
