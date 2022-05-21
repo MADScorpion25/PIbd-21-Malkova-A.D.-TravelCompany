3e667811c5ef8922eae290024fc1476179f70070
Malkova Anastasia <ad.malkova73@gmail.com>
2022-05-21 11:57:39 +0400

    CompLab8 ready

--- TravelCompany/TravelCompanyContracts/ViewModels/WarehouseViewModel.cs
+++ TravelCompany/TravelCompanyContracts/ViewModels/WarehouseViewModel.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using TravelCompanyContracts.Attributes;
 
 namespace TravelCompanyContracts.ViewModels
 {
     public class WarehouseViewModel
     {
         public int Id { get; set; }
-        [DisplayName("Название склада")]
+        [Column(title: "Название склада", width: 100)]
         public string WarehouseName { get; set; }
-        [DisplayName("ФИО ответственного")]
+        [Column(title: "ФИО ответственного", width: 50)]
         public string ResponsibleFullName { get; set; }
-        [DisplayName("Дата создания")]
+        [Column(title: "Дата создания", width: 100, dateFormat: "d")]
         public DateTime CreateDate { get; set; }
         public Dictionary<int, (string, int)> WarehouseConditions { get; set; }
     }
